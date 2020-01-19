7468dcb38756a595ff7ef833fa00d1c2009460e6
linkji <linkji@linkjideiMac.local>
2020-01-19 15:59:50 +0800

    fix

--- blzo-ex-basic/src/main/java/com/jdkhome/blzo/ex/basic/pojo/ApiResponse.java
+++ blzo-ex-basic/src/main/java/com/jdkhome/blzo/ex/basic/pojo/ApiResponse.java
@@ -63,11 +63,8 @@ public class ApiResponse<T> {
      * @param se
      */
     public ApiResponse(ServiceException se) {
-        this.code = se.getBaseError().getCode();
-        this.msg = se.getBaseError().getMsg();
-        if (!se.getErrorCode().equals(se.getBaseError().getCode())) {
-            this.msg = this.msg + "->" + se.getBaseError();
-        }
+        this.code = se.getErrorCode();
+        this.msg = se.getErrorMsg();
         this.debug = se.getDebug();
     }
 
@@ -75,7 +72,7 @@ public class ApiResponse<T> {
         this.code = se.getErrorCode();
         this.msg = se.getBaseError().getMsg(i18n);
         if (!se.getErrorCode().equals(se.getBaseError().getCode())) {
-            this.msg = this.msg + "->" + se.getBaseError();
+            this.msg = se.getErrorMsg();
         }
         this.debug = se.getDebug();
     }
