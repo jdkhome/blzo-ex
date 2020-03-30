76fe900356ce5c3ceb5ce19f3267c35c56f00eee
linkji <linkji@link-iMac.local>
2020-03-30 10:16:02 +0800

    version todo list

---
+++ app-demo/src/main/java/com/jdkhome/demo/controller/RiskDemoController.java
@@ -0,0 +1,15 @@
+package com.jdkhome.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * create by linkji on 12:12 下午 2020/3/27
+ * 简易风控服务演示
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/user_signin/demo")
+public class RiskDemoController {
+}
