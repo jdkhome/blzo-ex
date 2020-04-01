69428ee1c8093225b812127ad12b8e20a8c44f98
linkji <linkji@link-iMac.local>
2020-04-01 16:44:02 +0800

    change upload

--- upload.sh
+++ upload.sh
@@ -5,10 +5,22 @@ gradle publishMavenJavaPublicationToSonatypeRepository
 cd ../blzo-ex-basic
 gradle publishMavenJavaPublicationToSonatypeRepository
 
+cd ../blzo-ex-google-auth
+gradle publishMavenJavaPublicationToSonatypeRepository
+
 cd ../blzo-ex-ip2region
 gradle publishMavenJavaPublicationToSonatypeRepository
 
+cd ../blzo-ex-mqtt
+gradle publishMavenJavaPublicationToSonatypeRepository
+
+cd ../blzo-ex-mybatis-plus
+gradle publishMavenJavaPublicationToSonatypeRepository
+
 cd ../blzo-ex-redission
+gradle publishMavenJavaPublicationToSonatypeRepository
+
+cd ../blzo-ex-risk
 gradle publishMavenJavaPublicationToSonatypeRepository
 
 cd ../blzo-ex-usignin
@@ -20,20 +32,6 @@ gradle publishMavenJavaPublicationToSonatypeRepository
 cd ../blzo-ex-version
 gradle publishMavenJavaPublicationToSonatypeRepository
 
-cd ../blzo-ex-risk
-gradle publishMavenJavaPublicationToSonatypeRepository
 
-cd ../blzo-ex-mqtt
-gradle publishMavenJavaPublicationToSonatypeRepository
 
-cd ../blzo-ex-consul
-gradle publishMavenJavaPublicationToSonatypeRepository
 
-cd ../blzo-ex-task
-gradle publishMavenJavaPublicationToSonatypeRepository
-
-cd ../blzo-ex-google-auth
-gradle publishMavenJavaPublicationToSonatypeRepository
-
-cd ../blzo-ex-mybatis-plus
-gradle publishMavenJavaPublicationToSonatypeRepository
